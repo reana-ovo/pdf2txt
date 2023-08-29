2d723c41edeb3540e3b732b2c4dec5c871d93b39
Reana <reana.ovo@gmail.com>
2023-08-29 18:17:50 +0800

    perf(converter): 优化 pdfjs 文档载入，缩短转换时间

--- src/converter.ts
+++ src/converter.ts
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import pdfjs from 'pdfjs-dist';
 import * as logger from './utils/logger.js';
-// import CMapReaderFactory from './patch/CMapReaderFactory.js';
 
 const MAX_PDFDOC_FUNCS = 100;
 
@@ -17,32 +16,26 @@ export const pdf2jsonPages = async (pdfFilePath: string, outputFolderPath: strin
   const pdfDocLoader = pdfjs.getDocument({
     url: pdfFilePath,
     // CMapReaderFactory,
-    cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist/cmaps/',
-    cMapPacked: true,
+    // cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist/cmaps/',
+    // cMapPacked: true,
     useWorkerFetch: true,
+    // fontExtraProperties: true,
   });
   const pdfDoc = await pdfDocLoader.promise;
   const pageAmount = pdfDoc.numPages;
-  pdfDocLoader.destroy();
 
   // Logger
   logger.updateLog(path.basename(pdfFilePath, '.pdf'), 'converting', pageAmount);
 
   // Convert every page of the pdf file to json
   return [...Array(Math.ceil(pageAmount / MAX_PDFDOC_FUNCS)).keys()].reduce(async (result, row) => {
-    const pdfDocLoader = pdfjs.getDocument({
-      url: pdfFilePath,
-      // CMapReaderFactory,
-      cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist@3.9.179/cmaps/',
-      cMapPacked: true,
-      useWorkerFetch: true,
-    });
-    const pdfDoc = await pdfDocLoader.promise;
+    // Wait previous row to complete
+    await result;
 
     // Divide task into rows to prevent exceeding the maximum of pdfDocLoader functions
     const rowResult = [
       ...Array(Math.min(pageAmount - row * MAX_PDFDOC_FUNCS, MAX_PDFDOC_FUNCS)).keys(),
-    ].reduce(async (result, index) => {
+    ].map(async (index) => {
       // Extract text content to json
       const page = row * MAX_PDFDOC_FUNCS + index + 1;
       const pdfPage = await pdfDoc.getPage(page);
@@ -60,6 +53,6 @@ export const pdf2jsonPages = async (pdfFilePath: string, outputFolderPath: strin
       // Check if the output file is written correctly
       return fs.readFileSync(outputFilePath, 'utf-8') === textContent && result;
     }, Promise.resolve(true));
-    return (await rowResult) && result;
+    return (await Promise.all(rowResult)).reduce((prev, curr) => prev && curr, true) && result;
   }, Promise.resolve(true));
 };
