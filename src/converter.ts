c472faad378eb90336b4e3bb8dfc563ac8ab0282
Reana <reana.ovo@gmail.com>
2023-09-08 20:53:03 +0800

    fix(converter): 修复读取 PDF 时错误会终止程序的问题

--- src/converter.ts
+++ src/converter.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import pdfjs from 'pdfjs-dist';
+import pdfjs, { PDFDocumentProxy } from 'pdfjs-dist';
 import * as logger from './utils/logger.js';
 
 const MAX_PDFDOC_FUNCS = 100;
@@ -12,49 +12,59 @@ export const pdf2jsonPages = async (pdfFilePath: string, outputFolderPath: strin
   // Create output folder
   fs.mkdirSync(outputFolderPath, { recursive: true });
 
-  // Get total pages number
-  const pdfDocLoader = pdfjs.getDocument({
-    url: pdfFilePath,
-    // CMapReaderFactory,
-    // cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist/cmaps/',
-    // cMapPacked: true,
-    useWorkerFetch: true,
-    // fontExtraProperties: true,
-  });
-  const pdfDoc = await pdfDocLoader.promise;
-  const pageAmount = pdfDoc.numPages;
+  try {
+    // Create PDF loader
+    const pdfDocLoader = pdfjs.getDocument({
+      url: pdfFilePath,
+      // CMapReaderFactory,
+      // cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist/cmaps/',
+      // cMapPacked: true,
+      useWorkerFetch: true,
+      // fontExtraProperties: true,
+    });
+    const pdfDoc = await pdfDocLoader.promise;
 
-  // Logger
-  logger.updateLog(path.basename(pdfFilePath, '.pdf'), 'converting', pageAmount);
+    // Get total pages number
+    const pageAmount = pdfDoc.numPages;
 
-  // Convert every page of the pdf file to json
-  return [...Array(Math.ceil(pageAmount / MAX_PDFDOC_FUNCS)).keys()].reduce(async (result, row) => {
-    // Wait previous row to complete
-    await result;
+    // Logger
+    logger.updateLog(path.basename(pdfFilePath, '.pdf'), 'converting', pageAmount);
 
-    // Divide task into rows to prevent exceeding the maximum of pdfDocLoader functions
-    const rowResult = [
-      ...Array(Math.min(pageAmount - row * MAX_PDFDOC_FUNCS, MAX_PDFDOC_FUNCS)).keys(),
-    ].map(async (index) => {
-      // Extract text content to json
-      const page = row * MAX_PDFDOC_FUNCS + index + 1;
-      const pdfPage = await pdfDoc.getPage(page);
+    // Convert every page of the pdf file to json
+    return [...Array(Math.ceil(pageAmount / MAX_PDFDOC_FUNCS)).keys()].reduce(
+      async (result, row) => {
+        // Wait previous row to complete
+        await result;
 
-      // Enable normalization
-      const textContent = JSON.stringify(
-        await pdfPage.getTextContent({ disableNormalization: false }),
-      );
+        // Divide task into rows to prevent exceeding the maximum of pdfDocLoader functions
+        const rowResult = [
+          ...Array(Math.min(pageAmount - row * MAX_PDFDOC_FUNCS, MAX_PDFDOC_FUNCS)).keys(),
+        ].map(async (index) => {
+          // Extract text content to json
+          const page = row * MAX_PDFDOC_FUNCS + index + 1;
+          const pdfPage = await pdfDoc.getPage(page);
 
-      // Output json to file
-      const outputFilePath = path.resolve(outputFolderPath, page + '.json');
-      fs.writeFileSync(outputFilePath, textContent);
+          // Enable normalization
+          const textContent = JSON.stringify(
+            await pdfPage.getTextContent({ disableNormalization: false }),
+          );
 
-      // Logging
-      logger.updateLog(path.basename(pdfFilePath, '.pdf'));
+          // Output json to file
+          const outputFilePath = path.resolve(outputFolderPath, page + '.json');
+          fs.writeFileSync(outputFilePath, textContent);
 
-      // Check if the output file is written correctly
-      return fs.readFileSync(outputFilePath, 'utf-8') === textContent && result;
-    }, Promise.resolve(true));
-    return (await Promise.all(rowResult)).reduce((prev, curr) => prev && curr, true) && result;
-  }, Promise.resolve(true));
+          // Logging
+          logger.updateLog(path.basename(pdfFilePath, '.pdf'));
+
+          // Check if the output file is written correctly
+          return fs.readFileSync(outputFilePath, 'utf-8') === textContent && result;
+        }, Promise.resolve(true));
+        return (await Promise.all(rowResult)).reduce((prev, curr) => prev && curr, true) && result;
+      },
+      Promise.resolve(true),
+    );
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 };
